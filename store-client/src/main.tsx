1bb071b2cfffe30e89c61d5cda474e4c69f6a7fc
Erik J Brown <erkjbro@erikjbrown.tech>
2022-10-12 22:24:11 -0500

    SEP-1: Small refactor of main react file

--- store-client/src/main.tsx
+++ store-client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </StrictMode>
+);
